fd3c9085086494cb4cd92237b180b67d7981b7a6
GabrielX <97820374+gaburielu@users.noreply.github.com>
2024-01-08 20:17:31 +0800

    Including all root directory files

---
+++ personalize.bundle.js
@@ -0,0 +1,19 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************!*\
+  !*** ./src/personalize.js ***!
+  \****************************/
+const toggleFooterButton = document.getElementById("footer-toggle");
+const footer = document.getElementById("hiddenFooter");
+
+function toggleFooter() {
+  footer.classList.toggle("hidden");
+  footer.classList.toggle("show");
+}
+
+toggleFooterButton.addEventListener("click", toggleFooter);
+
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGVyc29uYWxpemUuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTs7QUFFQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Byb2plY3Qtd2VhdGhlci1hcHAvLi9zcmMvcGVyc29uYWxpemUuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgdG9nZ2xlRm9vdGVyQnV0dG9uID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJmb290ZXItdG9nZ2xlXCIpO1xuY29uc3QgZm9vdGVyID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJoaWRkZW5Gb290ZXJcIik7XG5cbmZ1bmN0aW9uIHRvZ2dsZUZvb3RlcigpIHtcbiAgZm9vdGVyLmNsYXNzTGlzdC50b2dnbGUoXCJoaWRkZW5cIik7XG4gIGZvb3Rlci5jbGFzc0xpc3QudG9nZ2xlKFwic2hvd1wiKTtcbn1cblxudG9nZ2xlRm9vdGVyQnV0dG9uLmFkZEV2ZW50TGlzdGVuZXIoXCJjbGlja1wiLCB0b2dnbGVGb290ZXIpO1xuXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
